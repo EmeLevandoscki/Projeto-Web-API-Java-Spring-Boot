47df233042184ff1b52b6f37def2c4ecfea56e3b
Emelly Levandoscki <99996497+EmeLevandoscki@users.noreply.github.com>
2025-07-15 21:38:31 -0300

    Model e DAO

--- projeto/src/main/java/br/com/criandoapi/projeto/controller/controllerusuario.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/controller/controllerusuario.java
@@ -1,14 +1,22 @@
 package br.com.criandoapi.projeto.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.criandoapi.projeto.DAO.IUsuario;
+import br.com.criandoapi.projeto.model.usuario;
 
 
 @RestController
 public class controllerusuario {
+
+    @Autowired IUsuario dao;
     
     @GetMapping("/usuarios")
-    public  String texto (){
-        return "Hello World";
+    public   List<usuario> ListaUsuarios (){
+        return (List<usuario>) dao.findAll();
     }
 }
